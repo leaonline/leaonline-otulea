6f04b7ff2743f110b92ee76908011f9090437232
Jan Küster <jkuester@uni-bremen.de>
2019-09-05 11:05:12 +0200

    ui component SoundButton toggle play with animated isPlaying state

--- imports/ui/components/soundbutton/soundbutton.js
+++ imports/ui/components/soundbutton/soundbutton.js
@@ -3,42 +3,62 @@ import { TTSEngine } from '../../../api/tts/TTSEngine'
 import { getBsType } from '../../../utils/bootstrapUtils'
 import './soundbutton.html'
 
-Template.soundbutton.helpers({
-  attributes () {
-    const instance = Template.instance()
-    const { data } = instance
+Template.soundbutton.onCreated(function () {
+  const instance = this
+  const { data } = instance
 
-    const btnType = getBsType(data.type, data.outline)
-    const btnBlock = data.block ? 'btn-block' : ''
-    const btnLg = data.lg ? 'btn-lg' : ''
-    const btnXl = data.xl ? 'btn-xl' : ''
-    const btnSm = data.sm ? 'btn-sm' : ''
-    const customClass = data.class || ''
+  const btnType = getBsType(data.type, data.outline)
+  const btnBlock = data.block ? 'btn-block' : ''
+  const btnLg = data.lg ? 'btn-lg' : ''
+  const btnXl = data.xl ? 'btn-xl' : ''
+  const btnSm = data.sm ? 'btn-sm' : ''
+  const customClass = data.class || ''
 
-    return {
-      id: data.id,
-      title: data.title,
-      class: `lea-sound-btn btn btn-${btnType} ${btnBlock} ${btnSm} ${btnLg} ${btnXl} ${customClass}`,
-      'data-tts': data.tts
-    }
-  }
+  instance.isPlaying = new ReactiveVar(false)
+  instance.attributes = new ReactiveVar({
+    id: data.id,
+    title: data.title,
+    class: `lea-sound-btn btn btn-${btnType} ${btnBlock} ${btnSm} ${btnLg} ${btnXl} ${customClass}`,
+    'data-tts': data.tts
+  })
 })
 
-function playEvent (event, templateInstance) {
-  const id = templateInstance.$(event.currentTarget).data('tts')
-  if (id) {
-    try {
-      TTSEngine.play({ id })
-    } catch (e) {
-      console.error(e)
-      // TODO noitfy?
+Template.soundbutton.helpers({
+  attributes () {
+    const isPlaying = Template.instance().isPlaying.get()
+    const atts = Object.assign({}, Template.instance().attributes.get())
+    if (isPlaying) {
+      atts.class += ' active'
     }
+    return atts
+  },
+  isPlaying () {
+    return Template.instance().isPlaying.get()
   }
-}
+})
 
 Template.soundbutton.events({
   'click .lea-sound-btn' (event, templateInstance) {
     event.preventDefault()
-    playEvent(event, templateInstance)
+
+    const isPlaying = templateInstance.isPlaying.get()
+
+    if (isPlaying) {
+      TTSEngine.stop()
+      templateInstance.isPlaying.set(false)
+      return
+    }
+
+    const id = templateInstance.$(event.currentTarget).data('tts')
+    const onEnd = () => templateInstance.isPlaying.set(false)
+    if (id) {
+      try {
+        TTSEngine.play({ id, onEnd })
+        templateInstance.isPlaying.set(true)
+      } catch (e) {
+        console.error(e)
+        // TODO noitfy?
+      }
+    }
   }
 })
