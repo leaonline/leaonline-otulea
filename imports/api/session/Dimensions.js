7a557bfdab59375d29667f46c77732ca51d13c52
Jan Kuester <jkuester@uni-bremen.de>
2019-09-09 15:59:31 +0200

    API session model Dimensions and Levels added

---
+++ imports/api/session/Dimensions.js
@@ -0,0 +1,49 @@
+export const Dimensions = {}
+
+Dimensions.writing = {
+  name: 'writing',
+  label: 'dimensions.writing.title',
+  icon: 'pen-alt',
+  type: 'primary',
+  descriptions: {
+    easy: 'dimensions.writing.easy',
+    medium: 'dimensions.writing.medium',
+    hard: 'dimensions.writing.hard'
+  }
+}
+
+Dimensions.reading = {
+  name: 'reading',
+  label: 'dimensions.reading.title',
+  icon: 'book-open',
+  type: 'danger',
+  descriptions: {
+    easy: 'dimensions.reading.easy',
+    medium: 'dimensions.reading.medium',
+    hard: 'dimensions.reading.hard'
+  }
+}
+
+Dimensions.math = {
+  name: 'math',
+  label: 'dimensions.math.title',
+  icon: 'calculator',
+  type: 'success',
+  descriptions: {
+    easy: 'dimensions.math.easy',
+    medium: 'dimensions.math.medium',
+    hard: 'dimensions.math.hard'
+  }
+}
+
+Dimensions.understand = {
+  name: 'understand',
+  label: 'dimensions.understand.title',
+  icon: 'comments',
+  type: 'warning',
+  descriptions: {
+    easy: 'dimensions.understand.easy',
+    medium: 'dimensions.understand.medium',
+    hard: 'dimensions.understand.hard'
+  }
+}
---
+++ imports/api/session/Levels.js
@@ -0,0 +1,16 @@
+export const Levels = {}
+
+Levels.easy = {
+  name: 'easy',
+  label: 'levels.easy'
+}
+
+Levels.medium = {
+  name: 'medium',
+  label: 'levels.medium'
+}
+
+Levels.hard = {
+  name: 'hard',
+  label: 'levels.hard'
+}
