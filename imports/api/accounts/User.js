3001a1aa8b74808ab1a9c88161aec2fedb0ffbff
Jan Küster <jkuester@uni-bremen.de>
2019-09-12 12:31:56 +0200

    api Users context created

---
+++ imports/api/accounts/User.js
@@ -0,0 +1,82 @@
+/* global Accounts Meteor */
+import { check } from 'meteor/check'
+import { onClient, onServer } from '../../utils/archUtils'
+import { Role } from './Role'
+import { Group } from './Group'
+
+export const Users = {
+  name: 'users',
+  label: 'users.title',
+  icon: 'users',
+  methods: {},
+  publications: {}
+}
+
+Users.schema = {
+  createdAt: Date,
+  username: String,
+  email: {
+    type: String,
+    optional: true
+  },
+  services: {
+    type: Object,
+    blackbox: true // TODO specify
+  },
+  agents: {
+    type: Array,
+    optional: true
+  },
+  'agents.$': Object,
+  'agents.$.name': String,
+  'agents.$.screenWidth': Number,
+  'agents.$.screenHeight': Number,
+  'agents.$.viewPortWidth': Number,
+  'agents.$.viewPortHeight': Number,
+
+}
+
+Users.methods.register = {
+  name: 'user.methods.register',
+  schema: {
+    code: String
+  },
+  isPublic: true,
+  numRequests: 1,
+  timeInterval: 1000,
+  run: onServer(function ({ code }) {
+    check(code, String)
+    const userId = Accounts.createUser({ username: code, password: code })
+    Roles.addUsersToRoles(userId, [ Role.runSession.value ], Group.field.value)
+    return userId
+  }),
+  call: onClient(function ({ code }, cb) {
+    Meteor.call(Users.methods.register.name, { code }, cb)
+  })
+}
+
+Users.methods.loggedIn = {
+  name: 'user.methods.loggedIn',
+  schema: {
+    screenWidth: Number,
+    screenHeight: Number,
+    viewPortWidth: Number,
+    viewPortHeight: Number
+  },
+  roles: [ Role.runSession.value, Role.test.value ],
+  group: Group.field.value,
+  numRequests: 1,
+  timeInterval: 1000,
+  run: onServer(function ({ screenWidth, screenHeight, viewPortWidth, viewPortHeight }) {
+    import { SHA256 } from 'meteor/sha'
+    const { userId } = this
+    const { connection } = this
+    const origin = SHA256(connection.clientAddress)
+    const name = connection.httpHeaders[ 'user-agent' ]
+    const updateDoc = { origin, name, screenWidth, screenHeight, viewPortWidth, viewPortHeight }
+    return Meteor.users.update(userId, { $addToSet: { agents: updateDoc } })
+  }),
+  call: onClient(function ({ screenWidth, screenHeight, viewPortWidth, viewPortHeight }, cb) {
+    Meteor.call(Users.methods.loggedIn.name, { screenWidth, screenHeight, viewPortWidth, viewPortHeight }, cb)
+  })
+}
