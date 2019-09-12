2f10630d36ad68109e803513fe9f613d1b680459
Jan Küster <jkuester@uni-bremen.de>
2019-09-12 23:03:13 +0200

    factories createCollection added

---
+++ imports/factories/collection/createCollection.js
@@ -0,0 +1,9 @@
+import { Mongo } from "meteor/mongo"
+import { Schema } from '../../api/schema/Schema'
+
+export const createCollection = ({ name, schema }) => {
+  const collection = new Mongo.Collection(name)
+  const collectionSchema = Schema.create(schema)
+  collection.attachSchema(collectionSchema)
+  return collection
+}
