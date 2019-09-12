ced1040a3615868bf4dc99d87d7881bd5f7ce365
Jan Küster <jkuester@uni-bremen.de>
2019-09-12 12:52:26 +0200

    api removed accounts methods from own file and merged into USers

--- imports/api/accounts/server/methods.js
+++
@@ -1,8 +0,0 @@
-import { check } from 'meteor/check'
-
-Meteor.methods({
-  'registerUser' ({ code }) {
-    check(code, String)
-    return Accounts.createUser({ username: code, password: code })
-  }
-})
