1feb5b78504313ad648f46d2180d2a5677943d19
jankapunkt <jkuester@uni-bremen.de>
2019-09-18 11:18:11 +0200

    ui not found page layout improved to card based

--- imports/ui/pages/notfound/notFound.js
+++ imports/ui/pages/notfound/notFound.js
@@ -1,3 +1,25 @@
+import { Template } from 'meteor/templating'
+import { dataTarget } from '../../../utils/eventUtils'
+import { loggedIn } from '../../../utils/accountUtils'
 import '../../components/textgroup/textgroup'
 import '../../components/actionButton/actionButton'
 import './notFound.html'
+
+Template.notFound.helpers({
+  hideDecision () {
+    if (!loggedIn()) return true
+    return Template.getState('hideDecision')
+  }
+})
+
+Template.notFound.events({
+  'click .lea-not-found-decision-button' (event, templateInstance) {
+    event.preventDefault()
+    const decision = dataTarget(event, templateInstance, 'decision')
+    if (decision) {
+      // TODO SEND NOT FOUND ERROR HERE
+    } else {
+      templateInstance.state.set('hideDecision', true)
+    }
+  }
+})
