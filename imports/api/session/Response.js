05c1a85c8c6c690c669863a1bdf1c63b3c2396e2
jankapunkt <jkuester@uni-bremen.de>
2019-09-19 12:14:21 +0200

    api Response implement send method

--- imports/api/session/Response.js
+++ imports/api/session/Response.js
@@ -1,3 +1,7 @@
+import { Role } from '../accounts/Role'
+import { Group } from '../accounts/Group'
+import { onClient, onServer } from '../../utils/archUtils'
+
 export const Response = {
   name: 'response',
   label: 'repsonse.title',
@@ -5,6 +9,7 @@ export const Response = {
 }
 
 Response.schema = {
+  sessionId: String,
   taskId: String,
   startedAt: String,
   completedAt: String,
@@ -12,4 +17,25 @@ Response.schema = {
   'answers.$:': Object,
   'answers.$.interactionId:': String,
   'answers.$.value:': String
+}
+
+Response.methods = {}
+
+Response.methods.send = {
+  name: 'response.methods.send',
+  schema: {
+    sessionId: String,
+    taskId: String,
+    answers: Response.schema.answers
+  },
+  roles: [Role.runSession.value, Role.test.value],
+  group: Group.field.value,
+  numRequests: 1,
+  timeInterval: 1000,
+  run: onServer(function ({ sessionId, taskId, answers }) {
+
+  }),
+  call: onClient(function ({ sessionId, taskId, answers }, cb) {
+    Meteor.call(Response.methods.send.name, {sessionId, taskId, answers }, cb)
+  })
 }
