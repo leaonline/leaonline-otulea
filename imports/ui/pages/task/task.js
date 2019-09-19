c345578018e7e442bc88d0ddaea348b7aaad1589
jankapunkt <jkuester@uni-bremen.de>
2019-09-19 09:37:32 +0200

    ui task page add finish button if no next page is present

--- imports/ui/pages/task/task.js
+++ imports/ui/pages/task/task.js
@@ -3,15 +3,17 @@ import { Session } from '../../../api/session/Session'
 import { Task } from '../../../api/session/Task'
 import { fadeOut } from '../../../utils/animationUtils'
 import { Router } from '../../../api/routing/Router'
+import { dataTarget } from '../../../utils/eventUtils'
 
 import '../../components/actionButton/actionButton'
 import './renderer/factory/TaskRendererFactory'
 import './task.html'
 
+
 Template.task.onCreated(function () {
   const instance = this
   const { taskId } = instance.data.params
-  const currentPage = Router.queryParam('s') || 0
+  const currentPageCount = Router.queryParam('p') || 0
 
   instance.autorun(() => {
     const taskDoc = instance.state.get('taskDoc')
@@ -36,8 +38,9 @@ Template.task.onCreated(function () {
   Task.helpers.load(taskId, (err, taskDoc) => {
     if (taskDoc) {
       instance.state.set('taskDoc', taskDoc)
-      instance.state.set('currentPage', taskDoc.pages[currentPage])
-      instance.state.set('hasNext', taskDoc.pages.length > currentPage)
+      instance.state.set('currentPageCount', currentPageCount)
+      instance.state.set('currentPage', taskDoc.pages[currentPageCount])
+      instance.state.set('hasNext', taskDoc.pages.length > currentPageCount + 1)
     } else {
       const route = instance.data.prev()
       fadeOut('.lea-task-container', instance, () => {
@@ -66,5 +69,36 @@ Template.task.helpers({
   },
   hasPrev () {
     return Template.getState('hasPrev')
+  }
+})
+
+Template.task.events({
+  'click .lea-pagenav-button' (event, templateInstance) {
+    event.preventDefault()
+    const action = dataTarget(event,templateInstance, 'action')
+    const taskDoc = templateInstance.state.get('taskDoc')
+    const currentPageCount = templateInstance.state.get('currentPageCount')
+    const newPage = {}
+
+    if (action === 'next') {
+      newPage.currentPageCount = currentPageCount + 1
+      newPage.currentPage = taskDoc.pages[newPage.currentPageCount]
+    } else if (action === 'back') {
+      newPage.currentPageCount = currentPageCount - 1
+      newPage.currentPage = taskDoc.pages[newPage.currentPageCount]
+    } else {
+      throw new Error(`Undefined action: ${action}`)
+    }
+
+    if (!newPage.currentPage) {
+      throw new Error(`Undefined page for current index ${newPage.currentPageCount}`)
+    }
+
+    templateInstance.state.set(newPage)
+  },
+  'click .lea-pagenav-finish-button' (event, templateInstance) {
+    event.preventDefault()
+
+
   }
 })
