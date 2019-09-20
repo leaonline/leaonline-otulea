c99a7e159545511364dcfb5ba8ec46f6b260f0ab
jankapunkt <jkuester@uni-bremen.de>
2019-09-20 17:10:07 +0200

    ui complete page implemented

--- imports/ui/pages/complete/complete.js
+++ imports/ui/pages/complete/complete.js
@@ -1,3 +1,32 @@
+import { Session } from '../../../api/session/Session'
 import '../../components/textgroup/textgroup'
+import '../../components/actionButton/actionButton'
 import './complete.html'
 
+Template.complete.onCreated(function () {
+  const instance = this
+
+  const { sessionId } = instance.data.params
+  Session.methods.results.call({sessionId}, (err, results) => {
+    if (err) {
+      return console.error(err) // TODO handle
+    }
+    instance.state.set('results', results)
+  })
+})
+
+Template.complete.helpers({
+  printOptions() {
+    return Template.getState('printOptions')
+  },
+  evaluationResults () {
+    return Template.getState('results')
+  }
+})
+
+Template.complete.events({
+  'click .lea-complete-forward-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('printOptions', true)
+  }
+})
