5e2554f5c04a2dd99347ab12e14bdf21311c055c
jankapunkt <jkuester@uni-bremen.de>
2019-09-23 08:16:20 +0200

    ui logout page logout user in onCreated

--- imports/ui/pages/logout/logout.js
+++ imports/ui/pages/logout/logout.js
@@ -1,2 +1,8 @@
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
 import '../../components/textgroup/textgroup'
 import './logout.html'
+
+Template.logout.onCreated(function () {
+  Meteor.logout(err => console.error(err))
+})
