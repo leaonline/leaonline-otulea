4fad968cee68af354c58a2d6e928f4a6a08c56f1
jankapunkt <jkuester@uni-bremen.de>
2019-09-23 08:07:13 +0200

    ui complete page states for results, print and decision implemented

--- imports/ui/pages/complete/complete.js
+++ imports/ui/pages/complete/complete.js
@@ -4,8 +4,21 @@ import '../../components/textgroup/textgroup'
 import '../../components/actionButton/actionButton'
 import './complete.html'
 
+const states = {
+  showResults: 'showResults',
+  showPrint: 'showPrint',
+  showDecision: 'showDecision'
+}
+
 Template.complete.onCreated(function () {
   const instance = this
+  const currentView=Router.queryParam('v')
+  if (currentView && states[currentView]) {
+    instance.state.set('view', currentView)
+  } else {
+    instance.state.set('view', states.showResults)
+  }
+
 
   const { sessionId } = instance.data.params
   Session.methods.results.call({sessionId}, (err, results) => {
@@ -22,11 +35,41 @@ Template.complete.helpers({
   },
   evaluationResults () {
     return Template.getState('results')
+  },
+  showResults() {
+    return Template.getState('view') === states.showResults
+  },
+  showDecision () {
+    return Template.getState('view') === states.showDecision
+  },
+  showPrint () {
+    return Template.getState('view') === states.showPrint
   }
 })
 
 Template.complete.events({
-  'click .lea-complete-forward-button' (event, templateInstance) {
+  'click .lea-showresults-forward-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('view', states.showPrint)
+  },
+  'click .lea-showprint-back-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('view', states.showResults)
+  },
+  'click .lea-showprint-forward-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('view', states.showDecision)
+  },
+  'click .lea-showdecision-back-button'(event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('view', states.showPrint)
+  },
+  'click .lea-end-button' (event, templateInstance) {
+    event.preventDefault()
+    const route = templateInstance.data.end()
+    Router.go(route)
+  },
+  'click .lea-continue-button' (event, templateInstance) {
     event.preventDefault()
     const route = templateInstance.data.next()
     Router.go(route)
