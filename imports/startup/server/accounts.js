23a10bc00b88fb5334c668d9732398d8b60048c6
Jan Kuester <jkuester@uni-bremen.de>
2019-10-15 16:11:08 +0200

    add accounts-lea and ddp-login-handler to allow backend user to authenticate

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -3,6 +3,7 @@ import { Accounts } from 'meteor/accounts-base'
 import { RateLimiterRegistry } from '../../factories/ratelimit/RateLimiterRegistry'
 import { createMethods } from '../../factories/method/createMethods'
 import { rateLimitMethods } from '../../factories/ratelimit/rateLimit'
+import { registerOAuthDDPLoginHandler } from 'meteor/leaonline:ddp-login-handler'
 
 //  //////////////////////////////////////////////////////////
 //  CUSTOM USERS METHODS
@@ -43,3 +44,26 @@ Accounts.removeDefaultRateLimit()
   'meteor_autoupdate_clientVersions',
   '_roles'
 ].forEach(name => RateLimiterRegistry.addPublication({ name, timeInterval: 10000, numRequests: 20 }))
+
+//  //////////////////////////////////////////////////////////
+//  LOGIN HANDLER FOR BACKEND
+//  //////////////////////////////////////////////////////////
+Meteor.startup(() => {
+  const { oauth } = Meteor.settings
+  ServiceConfiguration.configurations.upsert(
+    { service: 'lea' },
+    {
+      $set: {
+        loginStyle: 'popup',
+        clientId: oauth.clientId,
+        secret: oauth.secret,
+        dialogUrl: oauth.dialogUrl,
+        accessTokenUrl: oauth.accessTokenUrl,
+        identityUrl: oauth.identityUrl,
+        redirectUrl: oauth.redirectUrl
+      }
+    }
+  )
+
+  registerOAuthDDPLoginHandler({ identityUrl: oauth.identityUrl })
+})
