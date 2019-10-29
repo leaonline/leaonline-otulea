d37a9b84fb115e4996af0d4bbaa115af2ed81f2b
jankapunkt <jkuester@uni-bremen.de>
2019-10-29 09:28:20 +0100

    configure TTS using LeaCoreLib

--- imports/startup/client/leaconfig.js
+++ imports/startup/client/leaconfig.js
@@ -1,4 +1,7 @@
 import {LeaCoreLib} from 'meteor/leaonline:corelib'
 import { i18n } from '../../api/i18n/I18n'
 
-LeaCoreLib.i18n.load(i18n)
+Meteor.startup(() => {
+  LeaCoreLib.i18n.load(i18n)
+  LeaCoreLib.ttsEngine.configure({ ttsUrl: Meteor.settings.public.tts.url })
+})
