cb6e33bbfeb3b6c4c5dc92fa3801052cd7f1e63c
Jan Küster <jkuester@uni-bremen.de>
2019-11-23 11:59:12 +0100

    language updated

--- imports/api/config/Videos.js
+++ imports/api/config/Videos.js
@@ -11,22 +11,27 @@ export const Videos = {
 Videos.schema = {
   welcome: {
     type: String,
+    label:"videos.welcome",
     optional: true
   },
   overview: {
     type: String,
+    label:"videos.overview",
     optional: true
   },
   task: {
     type: String,
+    label:"videos.task",
     optional: true
   },
   complete: {
     type: String,
+    label:"videos.complete",
     optional: true
   },
   notFound: {
     type: String,
+    label:"videos.notFound",
     optional: true
   }
 }
