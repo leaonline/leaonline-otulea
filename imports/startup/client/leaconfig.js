303c03b3e0382fd3658e66d07c99c9094420359d
Jan Küster <jkuester@uni-bremen.de>
2019-11-23 22:03:02 +0100

    corrected export and removed unused file

--- imports/startup/client/leaconfig.js
+++ imports/startup/client/leaconfig.js
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor'
 import { LeaCoreLib } from '../../api/core/LeaCoreLib'
 import { i18n } from '../../api/i18n/I18n'
+import Detector from 'detect-os'
 
 Meteor.startup(() => {
+  const detector = new Detector()
+  detector.detect()
+  console.log(detector.detected)
+
   LeaCoreLib.i18n.load(i18n)
   LeaCoreLib.ttsEngine.configure({ ttsUrl: Meteor.settings.public.tts.url })
 })
