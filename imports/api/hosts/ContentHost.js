81f72576aaa580474209e09070f1adec574c229c
jankapunkt <jkuester@uni-bremen.de>
2019-11-23 09:55:55 +0100

    api ContentHost added

---
+++ imports/api/hosts/ContentHost.js
@@ -0,0 +1,23 @@
+import { HTTP } from 'meteor/http'
+const content = Meteor.settings.public.hosts.content
+const baseUrl = content.base
+const competencyUrl = `${baseUrl}${content.competency}`
+
+export const ContentHost = {}
+
+ContentHost.methods = {}
+
+ContentHost.methods.getCompetencies = function (competencies = []) {
+  return new Promise(resolve => {
+    HTTP.get(competencyUrl, {
+      data: { ids: competencies }
+    }, (err, res) => {
+      if (err) {
+        console.error(err)
+        resolve(null)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+}
