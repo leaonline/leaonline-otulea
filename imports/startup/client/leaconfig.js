5a302b274c2d027ab64cb8d09cb581a97432980b
Jan Kuester <jkuester@uni-bremen.de>
2019-12-06 15:23:29 +0100

    ui detect-os updated version and used in try/catch

--- imports/startup/client/leaconfig.js
+++ imports/startup/client/leaconfig.js
@@ -4,22 +4,30 @@ import { i18n } from '../../api/i18n/I18n'
 import Detector from 'detect-os'
 
 Meteor.startup(() => {
-  const detector = new Detector()
-  detector.detect()
-  const detected = detector.detected
-  const types = Detector.types
   let mode
-  switch (detected.os) {
-    case types.macos.os:
-    case types.windows.os:
-    case types.ios.os:
-    case types.android.os:
-      mode = LeaCoreLib.ttsEngine.modes.browser
-      break
-    default:
-      mode = LeaCoreLib.ttsEngine.modes.server
+  try {
+    const detector = new Detector()
+    detector.detect()
+    const detected = detector.detected
+    const types = Detector.types
+    let mode
+    switch (detected.os) {
+      case types.macos.os:
+      case types.windows.os:
+      case types.ios.os:
+      case types.android.os:
+        mode = LeaCoreLib.ttsEngine.modes.browser
+        break
+      default:
+        mode = LeaCoreLib.ttsEngine.modes.server
+    }
+  } catch (error) {
+    console.error('Failed to detect os, use fallback')
+    console.error(window.navigator.userAgent)
+    console.error(window.navigator.platform)
+    console.error(error)
+    mode = LeaCoreLib.ttsEngine.modes.server
   }
-
   LeaCoreLib.i18n.load(i18n)
   LeaCoreLib.ttsEngine.configure({ ttsUrl: Meteor.settings.public.tts.url, mode })
 })
