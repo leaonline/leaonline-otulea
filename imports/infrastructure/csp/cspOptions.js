8eb9938955bc9f04df16763a397835876e31ff71
Jan Küster <jkuester@uni-bremen.de>
2021-03-23 16:42:04 +0100

    api csp updated

--- imports/infrastructure/csp/cspOptions.js
+++ imports/infrastructure/csp/cspOptions.js
@@ -1,31 +1,12 @@
 /* global __meteor_runtime_config__ */
 import { Meteor } from 'meteor/meteor'
-import { check } from 'meteor/check'
 import crypto from 'crypto'
 import { WebApp } from 'meteor/webapp'
 import { Autoupdate } from 'meteor/autoupdate'
 
-const self = '\'self\''
-const data = 'data:'
-
-const getConnectSrc = url => {
-  const domain = url.replace(/http(s)*:\/\//, '').replace(/\/$/, '')
-  const s = url.match(/(?!=http)s(?=:\/\/)/) ? 's' : ''
-  const connectSrc = [
-    self,
-    `http${s}://${domain}`,
-    `ws${s}://${domain}`
-  ]
-
-  return { domain, s, connectSrc }
-}
-
-export function helmetOptions (externalHostUrls = [], print) {
-  const { s, connectSrc } = getConnectSrc(Meteor.absoluteUrl())
-  const connectSources = connectSrc.concat(externalHostUrls)
-  const imageSources = [self, data, 'blob:'].concat(externalHostUrls)
-  check(connectSources, [String])
-
+export function helmetOptions () {
+  const domain = Meteor.absoluteUrl().replace(/http(s)*:\/\//, '').replace(/\/$/, '')
+  const s = Meteor.absoluteUrl().match(/(?!=http)s(?=:\/\/)/) ? 's' : ''
   const runtimeConfig = Object.assign(__meteor_runtime_config__, Autoupdate, { accountsConfigCalled: true })
 
   // add missing client versions to runtimeconfig
@@ -45,34 +26,50 @@ export function helmetOptions (externalHostUrls = [], print) {
 
   const opt = {
     contentSecurityPolicy: {
+      browserSniff: false,
       blockAllMixedContent: true,
       directives: {
-        defaultSrc: [self],
+        childSrc: ["'self'"],
+        connectSrc: [
+          "'self'",
+          `http${s}://${domain}`,
+          `ws${s}://${domain}`
+        ],
+        defaultSrc: ["'self'"],
+        fontSrc: [
+          "'self'",
+          'data:'
+        ],
+        formAction: ["'self'"],
+        frameAncestors: ["'self'"],
+        frameSrc: ['*'],
+        imgSrc: [
+          "'self'",
+          'data:',
+          'blob:'
+        ],
+        manifestSrc: ["'self'"],
+        mediaSrc: ["'self'"],
+        objectSrc: ["'self'"],
+        sandbox: [
+          'allow-same-origin',
+          'allow-orientation-lock',
+          'allow-pointer-lock',
+          'allow-popups',
+          'allow-scripts',
+          'allow-forms',
+          'allow-modals',
+          //'allow-presentation',
+          //'allow-top-navigation',
+          'allow-popups-to-escape-sandbox'
+        ],
         scriptSrc: [
-          self,
+          "'self'",
           "'unsafe-eval'",
           `'sha256-${runtimeConfigHash}'`
         ],
-        childSrc: [self],
-        connectSrc: connectSources,
-        fontSrc: [self, data],
-        formAction: [self],
-        frameAncestors: [self],
-        frameSrc: ['*'],
-        imgSrc: imageSources,
-        manifestSrc: [self],
-        mediaSrc: [self],
-        objectSrc: [self],
-        sandbox: [
-          'allow-same-origin',
-          'allow-scripts',
-          'allow-forms',
-          // 'allow-popups',
-          // 'allow-popups-to-escape-sandbox'
-          'allow-modals'
-        ],
-        styleSrc: [self, "'unsafe-inline'"],
-        workerSrc: [self, 'blob:']
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        workerSrc: ["'self'", 'blob:']
       }
     },
     strictTransportSecurity: {
@@ -88,7 +85,7 @@ export function helmetOptions (externalHostUrls = [], print) {
       maxAge: 604800
     },
     frameguard: {
-      action: 'deny'
+      action: 'sameorigin'
     },
     dnsPrefetchControl: {
       allow: false
@@ -103,8 +100,5 @@ export function helmetOptions (externalHostUrls = [], print) {
     opt.contentSecurityPolicy.directives.scriptSrc = ['\'self\'', '\'unsafe-eval\'', '\'unsafe-inline\'']
   }
 
-  if (print) {
-    console.info(opt)
-  }
   return opt
 }
