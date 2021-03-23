27acc1c4ca345183df82c4b67ecc89c0d7e962d8
Jan Küster <jkuester@uni-bremen.de>
2021-03-23 17:13:33 +0100

    csp updated

--- imports/infrastructure/csp/cspOptions.js
+++ imports/infrastructure/csp/cspOptions.js
@@ -1,12 +1,31 @@
 /* global __meteor_runtime_config__ */
 import { Meteor } from 'meteor/meteor'
+import { check } from 'meteor/check'
 import crypto from 'crypto'
 import { WebApp } from 'meteor/webapp'
 import { Autoupdate } from 'meteor/autoupdate'
 
-export function helmetOptions () {
-  const domain = Meteor.absoluteUrl().replace(/http(s)*:\/\//, '').replace(/\/$/, '')
-  const s = Meteor.absoluteUrl().match(/(?!=http)s(?=:\/\/)/) ? 's' : ''
+const self = '\'self\''
+const data = 'data:'
+
+const getConnectSrc = url => {
+  const domain = url.replace(/http(s)*:\/\//, '').replace(/\/$/, '')
+  const s = url.match(/(?!=http)s(?=:\/\/)/) ? 's' : ''
+  const connectSrc = [
+    self,
+    `http${s}://${domain}`,
+    `ws${s}://${domain}`
+  ]
+
+  return { domain, s, connectSrc }
+}
+
+export function helmetOptions (externalHostUrls = [], print) {
+  const { s, connectSrc } = getConnectSrc(Meteor.absoluteUrl())
+  const connectSources = connectSrc.concat(externalHostUrls)
+  const imageSources = [self, data, 'blob:'].concat(externalHostUrls)
+  check(connectSources, [String])
+
   const runtimeConfig = Object.assign(__meteor_runtime_config__, Autoupdate, { accountsConfigCalled: true })
 
   // add missing client versions to runtimeconfig
@@ -19,57 +38,41 @@ export function helmetOptions () {
   })
 
   __meteor_runtime_config__.isModern = true
-  __meteor_runtime_config__.accountsConfigCalled = true // may vary, depending on your congfig
+  // __meteor_runtime_config__.accountsConfigCalled = true // may vary, depending on your congfig
 
   const runtimeConfigScript = `__meteor_runtime_config__ = JSON.parse(decodeURIComponent("${encodeURIComponent(JSON.stringify(runtimeConfig))}"))`
   const runtimeConfigHash = crypto.createHash('sha256').update(runtimeConfigScript).digest('base64')
 
   const opt = {
     contentSecurityPolicy: {
-      browserSniff: false,
       blockAllMixedContent: true,
       directives: {
-        childSrc: ["'self'"],
-        connectSrc: [
-          "'self'",
-          `http${s}://${domain}`,
-          `ws${s}://${domain}`
-        ],
-        defaultSrc: ["'self'"],
-        fontSrc: [
-          "'self'",
-          'data:'
-        ],
-        formAction: ["'self'"],
-        frameAncestors: ["'self'"],
-        frameSrc: ['*'],
-        imgSrc: [
-          "'self'",
-          'data:',
-          'blob:'
-        ],
-        manifestSrc: ["'self'"],
-        mediaSrc: ["'self'"],
-        objectSrc: ["'self'"],
-        sandbox: [
-          'allow-same-origin',
-          'allow-orientation-lock',
-          'allow-pointer-lock',
-          'allow-popups',
-          'allow-scripts',
-          'allow-forms',
-          'allow-modals',
-          // 'allow-presentation',
-          // 'allow-top-navigation',
-          'allow-popups-to-escape-sandbox'
-        ],
+        defaultSrc: [self],
         scriptSrc: [
-          "'self'",
+          self,
           "'unsafe-eval'",
           `'sha256-${runtimeConfigHash}'`
         ],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        workerSrc: ["'self'", 'blob:']
+        childSrc: [self],
+        connectSrc: connectSources,
+        fontSrc: [self, data],
+        formAction: [self],
+        frameAncestors: [self],
+        frameSrc: ['*'],
+        imgSrc: imageSources,
+        manifestSrc: [self],
+        mediaSrc: [self],
+        objectSrc: [self],
+        sandbox: [
+          'allow-same-origin',
+          'allow-scripts',
+          'allow-forms',
+          // 'allow-popups',
+          // 'allow-popups-to-escape-sandbox'
+          'allow-modals'
+        ],
+        styleSrc: [self, "'unsafe-inline'"],
+        workerSrc: [self, 'blob:']
       }
     },
     strictTransportSecurity: {
@@ -100,5 +103,8 @@ export function helmetOptions () {
     opt.contentSecurityPolicy.directives.scriptSrc = ['\'self\'', '\'unsafe-eval\'', '\'unsafe-inline\'']
   }
 
+  if (print) {
+    console.info(opt)
+  }
   return opt
 }
