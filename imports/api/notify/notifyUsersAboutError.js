f7b8c438fd11d31502b1804f5468f19e9d4c33c6
jankapunkt <jkuester@uni-bremen.de>
2022-10-04 10:52:12 +0200

    feature(client): improved logs for client events (csp;fonts;window)

--- imports/api/notify/notifyUsersAboutError.js
+++ imports/api/notify/notifyUsersAboutError.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 import { Email } from 'meteor/email'
 
-const appName = Meteor.settings.public.app.label
+const appName = Meteor.settings.public.app.name
 const { notify, replyTo, from } = Meteor.settings.email
 
 export const notifyUsersAboutError = error => {
@@ -10,7 +10,7 @@ export const notifyUsersAboutError = error => {
   notify.forEach(address => {
     Email.send({
       to: address,
-      subject: `${appName} [error]: ${error.type} - ${error.name}`,
+      subject: `${appName} [error]: ${error.message}`,
       replyTo: replyTo,
       from: from,
       text: JSON.stringify(error, null, 2)
