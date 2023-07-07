940f2a37292ce58f666c838896223b9f9b2e2ab1
jankapunkt <jkuester@uni-bremen.de>
2023-07-07 13:56:21 +0200

    core: remove unused code

--- imports/utils/number/validNumbers.js
+++ imports/utils/number/validNumbers.js
@@ -1,11 +1,3 @@
 export const isValidNumber = num => (typeof num === 'number') && !Number.isNaN(num)
 
 export const isValidInteger = num => isValidNumber(num) && Number.isSafeInteger(num)
-
-export const isValidFloat = num => {
-  if (!isValidNumber(num)) return false
-  if (num === 0) return true
-  return isInFloatBounds(num < 0 ? -num : num)
-}
-
-const isInFloatBounds = num => num >= Number.MIN_VALUE && num <= Number.MAX_VALUE
